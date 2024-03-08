c0a9ff736a32415be6e9e7154a9d971f0c26bbc9
Luca Bösch <luca.boesch@bfh.ch>
2024-03-08 11:02:08 +0100

    Localizations by Luca.

--- packages/react-app/src/webapp/ui/components/pages/Extra/Fallback/Fallback.tsx
+++ packages/react-app/src/webapp/ui/components/pages/Extra/Fallback/Fallback.tsx
@@ -3,6 +3,7 @@ import { ReactComponent as HatLogo } from '../../../../assets/icons/hat-moodle.s
 import type { MainLayoutProps } from '../../../layout/MainLayout/MainLayout.js'
 import MainLayout from '../../../layout/MainLayout/MainLayout.js'
 import './Fallback.scss'
+import { t } from '@moodlenet/react-app/webapp'
 
 export type FallbackProps = {
   mainLayoutProps: MainLayoutProps
@@ -12,7 +13,7 @@ export const Fallback: FC<FallbackProps> = ({ mainLayoutProps }) => {
   return (
     <MainLayout {...mainLayoutProps}>
       <div className="fallback-page">
-        <div className="content">Page not found or access not allowed</div>
+        <div className="content">{t('page_not_found_or_access_not_allowed')}</div>
         <div className="hat-logo">
           <HatLogo />
         </div>
