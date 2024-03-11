867f65eaeb749bd5ba80a74419df509ba02f221c
Stephan Robotta <stephan.robotta@bfh.ch>
2024-03-11 10:29:48 +0100

    Fix import in components

--- packages/component-library/src/webapp/ui/components/atoms/Searchbox/Searchbox.stories.tsx
+++ packages/component-library/src/webapp/ui/components/atoms/Searchbox/Searchbox.stories.tsx
@@ -1,6 +1,6 @@
+import { t } from '@moodlenet/react-app/webapp'
 import { action } from '@storybook/addon-actions'
 import type { Meta as ComponentMeta, StoryFn as ComponentStory } from '@storybook/react'
-import { t } from '@moodlenet/react-app/webapp'
 import type { SearchboxProps } from './Searchbox.js'
 import { Searchbox } from './Searchbox.js'
 
@@ -14,10 +14,10 @@ const meta: ComponentMeta<typeof Searchbox> = {
 }
 
 export const SearchboxStoryProps: SearchboxProps = {
-  placeholder: {t('start_type_to_search')},
+  placeholder: t('start_type_to_search'),
   searchText: '',
-  setSearchText: action('{t('search_text')}'),
-  search: action('{t('search')}'),
+  setSearchText: action('Search text'),
+  search: action('Search'),
 }
 
 const SearchboxStory: ComponentStory<typeof Searchbox> = args => <Searchbox {...args} />
