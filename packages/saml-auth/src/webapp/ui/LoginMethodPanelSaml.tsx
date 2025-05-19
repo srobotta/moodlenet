a3ebc41ab74704757239231a060b2c44980761c8
Stephan Robotta <stephan.robotta@bfh.ch>
2025-05-19 22:17:32 +0200

    Fix label in Saml-Auth login button.

--- packages/saml-auth/src/webapp/ui/LoginMethodPanelSaml.tsx
+++ packages/saml-auth/src/webapp/ui/LoginMethodPanelSaml.tsx
@@ -1,17 +1,24 @@
 import { PrimaryButton } from '@moodlenet/component-library'
+import { getCurrentLang } from '@moodlenet/react-app/common'
 import type { FC } from 'react'
 
-const label = 'Log in via Switch EDU-ID!'
+const label: Record<string, string> = {
+  de: 'Anmeldung über Switch edu-ID',
+  en: 'Log in using Switch edu-ID',
+  fr: 'Connectez-vous avec Switch edu-ID',
+}
+
+const lang = getCurrentLang()
 
 export const LoginButton: FC = () => {
-  return <PrimaryButton color="blue">{label}</PrimaryButton>
+  return <PrimaryButton color="blue">{label[lang] ?? label.en}</PrimaryButton>
 }
 
 export const LoginMethodPanelSaml: FC = () => {
   return (
     <div>
       <PrimaryButton color="blue">
-        <a href="/.pkg/@citricity/saml-auth/login">{label}</a>
+        <a href="/.pkg/@citricity/saml-auth/login">{label[lang] ?? label.en}</a>
       </PrimaryButton>
     </div>
   )
